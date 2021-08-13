9100f2d73f42c627c2926992391349b7bf1ddfcf
Envel <n.gutsenkov@tuta.io>
2021-08-13 23:18:01 +0300

    stable fixes

--- Prism/src/main/java/me/botsko/prism/commands/FlagsCommand.java
+++ Prism/src/main/java/me/botsko/prism/commands/FlagsCommand.java
@@ -50,9 +50,10 @@ public class FlagsCommand implements SubHandler {
                 .playerMsg(Il8nHelper.getMessage("flag-help-2")));
         for (final Flag flag : Flag.values()) {
             Prism.messenger.sendMessage(s,Prism.messenger.playerMsg(
-                    Component.text(flag.getUsage().replace("_", "-"))
+                    Component.text(flag.getUsage().replace("_", "-") + " ")
                             .color(NamedTextColor.LIGHT_PURPLE)
-                            .append(Component.text(" " + Il8nHelper.getMessage(flag.getDescription())))));
+                            //.append(Component.text(" " + Il8nHelper.getMessage(flag.getDescription())))));
+                            .append(Il8nHelper.getMessage(flag.getDescription()))));
         }
     }
 }
